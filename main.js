51e7fa7f0ab35db4b97d89d64c80df5ddda773fd
zakwan93 <zakwan_bhaiyat@yahoo.com>
2019-07-27 23:26:37 -0400

    Added CSS Preloder animation

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 // Detect request animation frame
-var scroll = window.requestAnimationFrame ||
-             // IE Fallback
-             function(callback){ window.setTimeout(callback, 1000/60)};
-var elementsToShow = document.querySelectorAll('.show-on-scroll'); 
+var scroll =
+  window.requestAnimationFrame ||
+  // IE Fallback
+  function(callback) {
+    window.setTimeout(callback, 1000 / 60);
+  };
+var elementsToShow = document.querySelectorAll(".show-on-scroll");
 
 function loop() {
-
-  elementsToShow.forEach(function (element) {
+  elementsToShow.forEach(function(element) {
     if (isElementInViewport(element)) {
-      element.classList.add('is-visible');
+      element.classList.add("is-visible");
     } else {
-      element.classList.remove('is-visible');
+      element.classList.remove("is-visible");
     }
   });
 
@@ -28,52 +30,57 @@ function isElementInViewport(el) {
   // }
   var rect = el.getBoundingClientRect();
   return (
-    (rect.top <= 0
-      && rect.bottom >= 0)
-    ||
-    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
-    ||
+    (rect.top <= 0 && rect.bottom >= 0) ||
+    (rect.bottom >=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <=
+        (window.innerHeight || document.documentElement.clientHeight)) ||
     (rect.top >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight))
   );
 }
-
 
 // To remove id of section from url onclick
 // Original JavaScript code by Chirp Internet: www.chirp.com.au
 
-window.addEventListener("DOMContentLoaded", function(e) {
-  var links = document.getElementsByTagName("A");
-  for(var i=0; i < links.length; i++) {
-    if(!links[i].hash) continue;
-    if(links[i].origin + links[i].pathname != self.location.href) continue;
-    (function(anchorPoint) {
-      links[i].addEventListener("click", function(e) {
-        anchorPoint.scrollIntoView(true);
-        e.preventDefault();
-      }, false);
-    })(document.getElementById(links[i].hash.replace(/#/, "")));
-  }
-}, false);
+window.addEventListener(
+  "DOMContentLoaded",
+  function(e) {
+    var links = document.getElementsByTagName("A");
+    for (var i = 0; i < links.length; i++) {
+      if (!links[i].hash) continue;
+      if (links[i].origin + links[i].pathname != self.location.href) continue;
+      (function(anchorPoint) {
+        links[i].addEventListener(
+          "click",
+          function(e) {
+            anchorPoint.scrollIntoView(true);
+            e.preventDefault();
+          },
+          false
+        );
+      })(document.getElementById(links[i].hash.replace(/#/, "")));
+    }
+  },
+  false
+);
 
+// Toggle on click
 
-// Toggle on click 
-
-var btn = document.getElementById('btn');
+var btn = document.getElementById("btn");
 // var show = document.getElementById("showless");
-var hidden = document.querySelectorAll('.hidden');
+var hidden = document.querySelectorAll(".hidden");
 
-btn.addEventListener("click", function(){
-	for(let i =0; i<hidden.length; i++){
-		hidden[i].classList.toggle("show");
-	}
-	// document.querySelector('.showless').classList.toggle("show");
-btn.innerHTML == "Show More" ? btn.innerHTML = "Show Less" : btn.innerHTML = "Show More";
-
+btn.addEventListener("click", function() {
+  for (let i = 0; i < hidden.length; i++) {
+    hidden[i].classList.toggle("show");
+  }
+  // document.querySelector('.showless').classList.toggle("show");
+  btn.innerHTML == "Show More"
+    ? (btn.innerHTML = "Show Less")
+    : (btn.innerHTML = "Show More");
 });
-
-
 
 //Mobile side-nav
 
@@ -81,11 +88,24 @@ var hamburgerButton = document.querySelector(".hamburger__button");
 var mobileNav = document.querySelector(".mobile");
 var header = document.querySelector(".header");
 
-hamburgerButton.addEventListener("click", function(){
-    mobileNav.classList.toggle('open');
+hamburgerButton.addEventListener("click", function() {
+  mobileNav.classList.toggle("open");
 });
 
-mobileNav.addEventListener("click", function(){
-    mobileNav.classList.remove("open");
-    header.classList.remove("open");
+mobileNav.addEventListener("click", function() {
+  mobileNav.classList.remove("open");
+  header.classList.remove("open");
+});
+
+// CSS LOADER
+
+function loading() {
+  document.getElementById("loading").style.display = "none";
+  document.getElementById("container").style.display = "grid";
+}
+
+window.addEventListener("load", function() {
+  setTimeout(() => {
+    loading();
+  }, 2000);
 });
